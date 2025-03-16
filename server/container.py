9011f48117194e79d255d14b051aa70f6f0a07d7
Robert Babaev <github@robertbabaev.tech>
2025-03-15 21:12:28 -0400

    add methods to container and image classes

--- server/container.py
+++ server/container.py
@@ -1,18 +1,27 @@
-from datetime import date
-import json
+import uuid
+import shutil
+from pathlib import Path
+from .image import Image
+
 
 class Container:
     def __init__(self, status: str, cpu: float, memory: int):
         self.status = status
         self.cpu = cpu
         self.memory = memory
-    
+        self.id = None
+
     def to_dict(self):
-        return {
-            "status": self.status,
-            "cpu": self.cpu,
-            "memory": self.memory
-        }
-    
+        return {"status": self.status, "cpu": self.cpu, "memory": self.memory}
+
     def __repr__(self):
         return f"Container(status={self.status}, cpu={self.cpu}, memory={self.memory})"
+
+    def prepare(self, container_image: Image):
+        image_dir = container_image.get_image_dir()
+        containers_dir = Path().home() / ".qnxtainer" / "containers"
+        container_id = uuid.uuid4().hex
+        container_dir = containers_dir / container_id
+        shutil.copytree(image_dir, container_dir)
+        self.id = container_id
+        return container_id
--- server/image.py
+++ server/image.py
@@ -1,5 +1,7 @@
+import tarfile
 from datetime import date
-import json
+from pathlib import Path
+
 
 class Image:
     def __init__(self, name: str, tag: str, created_at: date, description: str):
@@ -7,14 +9,27 @@ class Image:
         self.tag = tag
         self.created_at = created_at
         self.description = description
-    
+
     def to_dict(self):
         return {
             "name": self.name,
             "tag": self.tag,
             "created_at": self.created_at.isoformat(),
-            "description": self.description
+            "description": self.description,
         }
-    
+
     def __repr__(self):
         return f"Image(name={self.name}, tag={self.tag}, created_at={self.created_at}, description={self.description})"
+
+    def get_image_dir(self) -> Path:
+        images_dir = Path().home() / ".qnxtainer" / "images"
+        image_dir = images_dir / self.image_name / self.image_tag
+        return image_dir
+
+    def unpack_from(self, image_file_name: Path) -> Path:
+        image_dir = self.get_image_dir()
+        image_dir.mkdir(parents=True, exist_ok=True)
+        with tarfile.open(image_file_name) as tar:
+            tar.extractall(image_dir)
+        return image_dir
+        return image_dir
